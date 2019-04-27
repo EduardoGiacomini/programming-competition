592e832c8f29881328d148d0b6641de872f7f85e
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2019-04-26 22:12:50 -0400

    Add 'SPOJ-QUADRAD2' to script#00

---
+++ script#00-introduction/SPOJ-QUADRAD2/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int value;
+
+        value = keyboard.nextInt();
+
+        System.out.printf("%d\n", (value * value));
+    }
+}
