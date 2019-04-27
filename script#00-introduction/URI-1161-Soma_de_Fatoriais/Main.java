ed0c151a6fccd721db6ce837cbe4a4dc4bd1fc49
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2019-04-26 21:59:19 -0400

    Add 'URI-1161' to script#00

---
+++ script#00-introduction/URI-1161-Soma_de_Fatoriais/Main.java
@@ -0,0 +1,33 @@
+// URL: https://www.urionlinejudge.com.br/judge/pt/runs/code/13969468
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String [] args) {
+		Scanner keyboard = new Scanner(System.in);
+		int value_a, value_b;
+		long factorial_a, factorial_b;
+
+		while (keyboard.hasNext()) {
+			value_a = keyboard.nextInt();
+			value_b = keyboard.nextInt();
+			factorial_a = 1;
+			factorial_b = 1;
+
+			if (value_a > 1) {
+				while(value_a > 1) {
+					factorial_a *= value_a;
+					value_a--;
+				}
+			}
+
+			if (value_b > 1) {
+				while(value_b > 1) {
+					factorial_b *= value_b;
+					value_b--;
+				}
+			}
+
+			System.out.printf("%d\n", (factorial_a + factorial_b));
+		}
+	}
+}
