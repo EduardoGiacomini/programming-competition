d2e1dcf31bade8d180a5275712dc3a50e1b33dbf
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2019-04-29 21:57:27 -0400

    Add 'SPOJ-JPNEU' to script#00

---
+++ script#00-introduction/SPOJ-JPNEU/Main.cpp
@@ -0,0 +1,17 @@
+// URL: https://br.spoj.com/problems/JPNEU/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    int expected_pressure, current_pressure;
+
+    cin >> expected_pressure >> current_pressure;
+
+    cout << expected_pressure - current_pressure << "\n";
+
+    return 0;
+}
